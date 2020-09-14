bd2344df87b6b7e4f7770521a572cad20343bc56
yangrui <yangrui@ultrapower.com.cn>
2020-09-14 14:03:52 +0800

    Initial Commit

---
+++ YRFloatingView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YRFloatingView
+//
+//  Created by edz on 2020/9/14.
+//  Copyright © 2020 EDZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ YRFloatingView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YRFloatingView
+//
+//  Created by edz on 2020/9/14.
+//  Copyright © 2020 EDZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
